cfb6a6dd1dae7657eb00a7ad8c7a212de235d278
Andrea Mele <andrea.mele.au@gmail.com>
2019-01-28 01:32:20 +1100

    make it better

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import './App.scss';
-import Homepage from './Homepage'
+import './Styling/App.scss';
+import Homepage from './Components/Homepage'
+
+// Technical Decisions
+
+// The initial thought was to create a more scalable and decoupled application, so to provide a more solid foundation for the future. Therefore, the state was governed by two entities, the App, and the Ratings, given that these are the two components which either interact with the user, or require outside data. In developing, I have realized some of the limits of Codepen, and such approach would have made a big negative impact on the reviewer, who would have been required to read at many files. Therefore, I opted to rewrite the application and change some of the MVC base intent in order to provide ease of readability. Given also the small size of the project, this would have been the better choice.
+
+// Posts are self contained, and once again, reflect these simple design decisions.
 
 class App extends Component {
   render() {
